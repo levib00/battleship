03050387a62b9333302bcd3b57fdd98b882cbfc5
levib00 <levib8@hotmail.com>
2023-01-12 22:57:13 -0500

    refactor

--- src/factories/ship-factory.js
+++ src/factories/ship-factory.js
@@ -1,11 +1,16 @@
 const Ship = (length) => {
   let hits = 0;
-  const hit = () => {
-    hits += 1;
-    return hits;
+
+  return {
+    length,
+    hit() {
+      hits += 1;
+      return hits;
+    },
+    isSunk() {
+      return length === hits;
+    },
   };
-  const isSunk = () => length === hits;
-  return { length, hit, isSunk };
 };
 // eslint-disable-next-line import/prefer-default-export
 export { Ship };
