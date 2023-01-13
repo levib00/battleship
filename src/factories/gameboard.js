19b64890ae0bcb96644066513b32ed66573f7ba2
levib00 <levib8@hotmail.com>
2023-01-12 22:55:23 -0500

    Add logic for attacking/sinking ships + refactor

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -1,6 +1,8 @@
 import { Ship } from './ship-factory';
-
+// TODO: prevent attacking the same spot twice.
 const Gameboard = () => {
+  let numberOfShips = 0;
+  let sunkShips = 0;
   const gameSpaces = () => {
     // creates coordinates for all grid spaces from 1, 1 - 8, 8
     const grid = [];
@@ -17,19 +19,43 @@ const Gameboard = () => {
 
   const gameSpace = gameSpaces();
 
-  const placeCarrier = (placeCoordinates, xOrY = 0) => {
-    const carrier = Ship(5);
-    let placement = gameSpace.findIndex((ship) => ship.coordinates === placeCoordinates);
-    for (let i = placement; i < 5; i += 1) {
-      gameSpace[placement].hasShip = carrier;
-      const tempArray = JSON.parse(gameSpace[placement].coordinates);
-      tempArray[xOrY] += 1;
-      const newPlaceCoordinates = JSON.stringify(tempArray);
-      placement = gameSpace.findIndex((ship) => ship.coordinates === newPlaceCoordinates);
-    }
-    return gameSpace[placement];
+  const missedAttacks = [];
+
+  return {
+    gameSpace,
+
+    placeCarrier(placeCoordinates, xOrY = 0) {
+      numberOfShips += 1;
+      const carrier = Ship(5);
+      let placement = gameSpace.findIndex((ship) => ship.coordinates === placeCoordinates);
+      for (let i = 0; i < 5; i += 1) {
+        gameSpace[placement].hasShip = carrier;
+        const tempArray = JSON.parse(gameSpace[placement].coordinates);
+        tempArray[xOrY] += 1;
+        const newPlaceCoordinates = JSON.stringify(tempArray);
+        placement = gameSpace.findIndex((ship) => ship.coordinates === newPlaceCoordinates);
+      }
+      return carrier;
+    },
+
+    receiveAttack(attackCoordinates) {
+      const attackSpace = gameSpace.findIndex((ship) => ship.coordinates === attackCoordinates);
+
+      if (gameSpace[attackSpace].hasShip) {
+        const hits = gameSpace[attackSpace].hasShip.hit();
+        if (gameSpace[attackSpace].hasShip.isSunk()) {
+          sunkShips += 1;
+        }
+        return hits;
+      }
+      missedAttacks.push(attackCoordinates);
+      return false;
+    },
+
+    isAllShipsSunk() {
+      return numberOfShips === sunkShips;
+    },
   };
-  return { gameSpace, placeCarrier };
 };
 // eslint-disable-next-line import/prefer-default-export
 export { Gameboard };
