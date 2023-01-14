1aaae47f4f476b4286130f74a21311241e56c17f
levib00 <levib8@hotmail.com>
2023-01-14 17:19:38 -0500

    tests players factories

---
+++ src/_tests/players.test.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+import { Players } from '../factories/players';
+
+test('Players can be initialized', () => {
+  const player = Players();
+  expect(player).toBeTruthy();
+});
+
+test('Initializing players also creates a gameboard as that player', () => {
+  const player = Players('dab');
+  expect(player.playerBoard).toBeTruthy()
+});
+
+test('Players can send attacks', () => {
+  const player = Players()
+  const computer = Players()
+  computer.playerBoard.placeCarrier('[1,1]')
+
+  expect(player.sendAttacks(computer, '[1,1]')).toBe(1)
+});
+
+test('computer player will select its own attacks', () => {
+  const player = Players();
+  const computer = Players()
+
+  computer.playerBoard.placeCarrier('[1,1]');
+
+  expect(computer.computerAttack(player)).toBe(false)
+});
+
+test('Players cannot attack the same coordinates twice', () => {
+  const player = Players();
+  const computer = Players();
+
+  player.sendAttacks(computer, '[1,1]')
+  expect(player.sendAttacks(computer, '[1,1]')).toBe(null)
+});
+
+
+
