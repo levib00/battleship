810b0aee652eeb597fbdbef89f6cb0744126dd4a
levib00 <levib8@hotmail.com>
2023-01-14 17:19:07 -0500

    Add factory to create players

---
+++ src/factories/players.js
@@ -0,0 +1,25 @@
+import { Gameboard } from './gameboard';
+
+const Players = () => {
+  const attacks = [];
+  const playerBoard = Gameboard();
+  return {
+    playerBoard,
+
+    sendAttacks(gameboard, coordinates) {
+      if (attacks.includes(coordinates)) {
+        return null;
+      }
+      attacks.push(coordinates);
+      return gameboard.playerBoard.receiveAttack(coordinates);
+    },
+
+    computerAttack(gameboard) {
+      const coordinates = () => Math.floor(Math.random() * 10) + 1;
+
+      return this.sendAttacks(gameboard, `[${coordinates()},${coordinates()}]`);
+    },
+  };
+};
+// eslint-disable-next-line import/prefer-default-export
+export { Players };
