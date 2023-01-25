3e29e29b77bfe90185165fcb3385c75700d25457
levib00 <levib8@hotmail.com>
2023-01-24 19:24:55 -0500

    prevent invalid placements and game loop

--- src/game.js
+++ src/game.js
@@ -5,9 +5,6 @@ const makePlayers = () => {
   const playerPlayer = Players();
   const cpuPlayer = Players();
 
-  playerPlayer.playerBoard.placeShip('[1,1]', 4);
-  cpuPlayer.playerBoard.placeShip('[1,1]', 4);
-
   return { playerPlayer, cpuPlayer };
 };
 
@@ -15,71 +12,129 @@ const getPlayerName = () => {
   console.log(true);
 };
 
-const placePlayerShips = (button, playerPlayer, index) => {
-  console.log(playerPlayer.playerBoard.gamepace[index].coordinates);
-  /*
-  placeShip on the coordinates of the clicked square.
-  remove listeners from all tiles that have ships.
-  prevent overlap
-  ^^gonna need to make test() work with DI
-  rename test()
-  update ship tiles to be blue.
-  get input for each ship one at a time
-  */
-};
-
-const placeCpuShips = (cpuPlayer) => {
-  const unavailableCpuIndices = [];
-  for (let i = 1; i <= 5; i += 1) {
-    // also exists in computerAttack function.
-    const makeCoordinates = () => Math.floor(Math.random() * (10 - i)) + 1;
-    const direction = Math.round(Math.random());
-    const newCoordinates = () => {
-      let coordinateX = null;
-      let coordinateY = null;
-      if (!direction) {
-        coordinateX = makeCoordinates(i);
-        coordinateY = makeCoordinates(0);
-      } else {
-        coordinateX = makeCoordinates(0);
-        coordinateY = makeCoordinates(i);
+const placeShips = () => {
+  const test = (coordinateX, coordinateY, direction, usedIndices, length) => {
+    // TODO: refactor, rename this, arr, some key, maybe i too.
+    const arr = [];
+    if (!direction) {
+      for (let x = coordinateX; x < coordinateX + length; x += 1) {
+        arr.push(`[${x},${coordinateY}]`);
       }
-      return { coordinateX, coordinateY };
-    };
-
-    const test = (coordinateX, coordinateY) => {
-      const arr = [];
-      if (!direction) {
-        for (let x = coordinateX; x < coordinateX + i; x += 1) {
-          arr.push(`[${x},${coordinateY}]`);
-        }
-      } else {
-        for (let y = coordinateY; y < coordinateY + i; y += 1) {
-          arr.push(`[${coordinateX},${y}]`);
-        }
-      }
-      const found = unavailableCpuIndices.some((r) => arr.indexOf(r) >= 0);
-      return found;
-    };
-
-    /*
-      while the coordinate or any of the ships coordinates overlaps with any of the
-      coordinates/indices in unavailable indices, keep generating new coordinates.
-      if horizontal, x needs to be <= 10 - x, if vertical y needs to be <= 10 - y
-    */
-    let coordinates = newCoordinates();
-    while (test(coordinates.coordinateX, coordinates.coordinateY)) {
-      coordinates = newCoordinates();
-    }
-    const coordinatesString = `[${coordinates.coordinateX},${coordinates.coordinateY}]`;
-    unavailableCpuIndices.push(coordinatesString);
-    if (i < 2) {
-      const j = 2;
-      cpuPlayer.placeShip(coordinatesString, j, direction);
     } else {
-      cpuPlayer.placeShip(coordinatesString, i, direction);
+      for (let y = coordinateY; y < coordinateY + length; y += 1) {
+        arr.push(`[${coordinateX},${y}]`);
+      }
     }
-  }
+    const found = usedIndices.some((r) => arr.indexOf(r) >= 0);
+    return found;
+  };
+  const playerGrid = document.getElementById('player').childNodes;
+  const placeCpuShips = (cpuPlayer) => {
+    const unavailableCpuIndices = [];
+    for (let j = 1; j <= 5; j += 1) {
+      // also exists in computerAttack function.
+      const makeCoordinates = () => Math.floor(Math.random() * (10 - j)) + 1;
+      const direction = Math.round(Math.random());
+      const newCoordinates = () => {
+        let coordinateX = null;
+        let coordinateY = null;
+        if (!direction) {
+          coordinateX = makeCoordinates(j);
+          coordinateY = makeCoordinates(0);
+        } else {
+          coordinateX = makeCoordinates(0);
+          coordinateY = makeCoordinates(j);
+        }
+        return { coordinateX, coordinateY };
+      };
+
+      let coordinates = newCoordinates();
+      while (test(
+        coordinates.coordinateX,
+        coordinates.coordinateY,
+        direction,
+        unavailableCpuIndices,
+        j,
+      )) {
+        coordinates = newCoordinates();
+      }
+      const coordinatesString = `[${coordinates.coordinateX},${coordinates.coordinateY}]`;
+      unavailableCpuIndices.push(coordinatesString);
+      if (!direction) {
+        for (let x = coordinates.coordinateX; x < coordinates.coordinateX + j; x += 1) {
+          unavailableCpuIndices.push(`[${x},${coordinates.coordinateY}]`);
+        }
+      } else {
+        for (let y = coordinates.coordinateY; y < coordinates.coordinateY + j; y += 1) {
+          unavailableCpuIndices.push(`[${coordinates.coordinateX},${y}]`);
+        }
+      }
+      if (j < 2) {
+        const length = 2;
+        cpuPlayer.playerBoard.placeShip(coordinatesString, length, direction);
+      } else {
+        cpuPlayer.playerBoard.placeShip(coordinatesString, j, direction);
+      }
+    }
+  };
+  let i = 5;
+  const usedPlayerIndices = [];
+  const placePlayerShips = (gridSquare, playerPlayer, index, cpuPlayer) => {
+    const { coordinates } = playerPlayer.playerBoard.gameSpace[index];
+    const coordinatesArray = JSON.parse(coordinates);
+    let coordinateX = coordinatesArray[0];
+    let coordinateY = coordinatesArray[1];
+    const direction = 0;
+    let incrementIndex = index;
+    let k = i;
+    if (k < 2) {
+      k = 2;
+    }
+
+    if ((!test(
+      coordinateX,
+      coordinateY,
+      direction,
+      usedPlayerIndices,
+      k,
+    ))
+     && (k > 0)
+     && ((!direction && (coordinateX + k <= 10))
+     || (direction && (coordinateY + k <= 10)))) {
+      playerPlayer.playerBoard.placeShip(coordinates, i, direction);
+      for (let j = k; j > 0; j -= 1) {
+          if (!direction) {
+            playerGrid[incrementIndex].setAttribute('class', 'grid-square blue');
+            usedPlayerIndices.push(playerPlayer.playerBoard.gameSpace[incrementIndex].coordinates);
+            incrementIndex += 1;
+          }
+      }
+
+      i -= 1;
+      if (i === 0) {
+        const oldElement = gridSquare.parentNode;
+        const newElement = oldElement.cloneNode(true);
+        oldElement.parentNode.replaceChild(newElement, oldElement);
+        // eslint-disable-next-line no-restricted-syntax
+        for (const square of newElement.children) {
+          if (square.classList.contains('clickable')) {
+            square.classList.remove('clickable');
+          }
+        }
+        placeCpuShips(cpuPlayer);
+      }
+    }
+    /*
+    placeShip on the coordinates of the clicked square.
+    remove listeners from all tiles that have ships.
+    prevent overlap
+    ^^gonna need to make test() work with DI
+    rename test()
+    update ship tiles to be blue.
+    get input for each ship one at a time
+    */
+  };
+  return { placePlayerShips, placeCpuShips };
 };
 
 const gameLoop = (playerPlayer, cpuPlayer, index) => {
@@ -88,6 +143,9 @@ const gameLoop = (playerPlayer, cpuPlayer, index) => {
   step 2: let player place ships.
   step 3: place cpu ships randomly.
   */
+  if (playerPlayer.playerBoard.isAllShipsSunk() || cpuPlayer.playerBoard.isAllShipsSunk()) {
+    return;
+  }
 
   const playerBoard = playerPlayer.playerBoard.gameSpace;
   const playerGrid = document.getElementById('player').childNodes;
@@ -122,4 +180,6 @@ const gameLoop = (playerPlayer, cpuPlayer, index) => {
   } while (Loop.isHit || Loop.isHit === null);
 };
 
-export { makePlayers, gameLoop, placeCpuShips };
+export {
+  makePlayers, gameLoop, placeShips,
+};
