f4527a1278bea0245a5c450b2ec20ead0be2f1de
levib00 <levib8@hotmail.com>
2023-01-24 19:26:51 -0500

    fix bug caused by no extension in import

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,5 @@
 import { assignGrids } from '../src/dom-manipulation.js';
-import { makePlayers } from '../src/game-loop.js';
+import { makePlayers } from '../src/game.js';
 
 window.onload = () => {
   assignGrids(makePlayers());
